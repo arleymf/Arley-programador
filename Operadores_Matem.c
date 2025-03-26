e1df8cae3a714ad59269a440150c47c84cb4eed4
arleymf <arleymf13@gmail.com>
2025-03-26 19:37:24 +0000

    inicio

---
+++ Operadores_Matem.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main (){
+
+     /*
+     Soma (+)
+     Subtração (-)
+     Multiplicação (*)
+     Divisão (/)
+     */
+
+     int numero1, numero2;
+     int soma, subtracao, multiplicação, divisao;
+
+     // Operação Soma
+     soma = numero1 + numero2;
+     
+     // Operação Subtração000
+     subtracao = numero1 - numero2;
+
+     // Operação multiplicação
+     multiplicação = numero1 * numero2;
+
+     divisao = numero1 / numero2;
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
