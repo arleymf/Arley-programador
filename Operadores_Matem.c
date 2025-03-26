ebf34534d1d716ab0d2161afe9a7d6a47b9c05cf
arleymf <arleymf13@gmail.com>
2025-03-26 19:50:28 +0000

    inicio

--- Operadores_Matem.c
+++ Operadores_Matem.c
@@ -12,15 +12,21 @@ int main (){
      int numero1, numero2;
      int soma, subtracao, multiplicação, divisao;
 
-     // Operação Soma
+     printf("Entre com o número 1: \n");
+     scanf("%d", &numero1);
+     printf("Entre com o número 2: \n");
+     scanf("%d", &numero2);
+     
+     // Operação  Soma
      soma = numero1 + numero2;
      
-     // Operação Subtração000
+     // Operação Subtração
      subtracao = numero1 - numero2;
 
      // Operação multiplicação
      multiplicação = numero1 * numero2;
 
+     // Operação divisão 
      divisao = numero1 / numero2;
 
 
